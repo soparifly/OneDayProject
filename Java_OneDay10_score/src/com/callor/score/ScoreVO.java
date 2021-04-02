0695738144ee7736fab63859ff8d5c302f5304cf
soparifly <soparifly@naver.com>
2021-04-02 09:24:23 +0900

    Ondeay

---
+++ Java_OneDay10_score/src/com/callor/score/ScoreVO.java
@@ -0,0 +1,15 @@
+package com.callor.score;
+
+public class ScoreVO {
+	public ScoreVO() {
+
+		private String name;
+		private String ;
+		private String name;
+		private String name;
+		private String name;
+		private String name;
+		
+
+	}
+}
---
+++ Java_OneDay10_score/src/module-info.java
@@ -0,0 +1,2 @@
+module Java_OneDay10_score {
+}
